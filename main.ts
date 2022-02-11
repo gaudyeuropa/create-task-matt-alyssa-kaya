3188ba768ca0bb86ea7947522f9b9bba64688aed
Matt <91627813+gaudyeuropa@users.noreply.github.com>
2022-02-11 12:51:19 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+function displayMenu (menuNumber: number) {
+    if (menuNumber == 1) {
+        for (let index = 0; index < 2; index++) {
+            veganRecommendation = veganCourses._pickRandom()
+            game.splash(veganRecommendation)
+            veganCourses.removeAt(veganCourses.indexOf(veganRecommendation))
+        }
+    } else if (menuNumber == 2) {
+    	
+    } else if (menuNumber == 3) {
+    	
+    }
+}
+let veganRecommendation = ""
+let veganCourses: string[] = []
+game.showLongText("Hi! Welcome to Viva la Special! We offer inclusive options for those with dietary restrictions!", DialogLayout.Bottom)
+let dietaryRestrictions = game.askForNumber("", 3)
 let chef = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . 7 3 3 3 3 3 3 3 . 
@@ -16,3 +33,11 @@ let chef = sprites.create(img`
     . . . 3 . 2 . . . 7 . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
+veganCourses = [
+"Vegan Chicken Nuggets",
+"Tofu Power Bowl",
+"Veggie Burger with side of fries",
+"Vegan Curry",
+"Red leaf"
+]
+displayMenu(dietaryRestrictions)
